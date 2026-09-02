68090325e7550211e5bdcfa6d64691416e01e4c2
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-02 11:16:54 +0530

    Scaffold Spring Boot project

---
+++ src/main/java/com/darkpixellabs/deepsweep/DeepSweepApplication.java
@@ -0,0 +1,11 @@
+package com.darkpixellabs.deepsweep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepSweepApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeepSweepApplication.class, args);
+    }
+}
